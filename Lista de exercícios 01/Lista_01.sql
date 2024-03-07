810e6b1c41c93a5c2adc7935ebb36b4ac3fdb8c4
manrick Lázaro <manricksantos@gmail.com>
2024-03-07 16:02:06 -0300

    resolve segunda questão da lista.

--- Lista de exercícios 01/Lista_01.sql
+++ Lista de exercícios 01/Lista_01.sql
@@ -6,3 +6,20 @@
 */
 
 SELECT CodCorretor as código, Corretor as nome, Comissao as "percentual de comissão" FROM Corretor ORDER BY Comissao DESC
+
+
+
+
+
+
+/*
+	2. Escreva um comando que exiba o código e a descrição dos imóveis com uma área construída
+	compreendida entre uma faixa (valor inicial e valor final) fornecida como parâmetro, ordenado
+	pela descrição do imóvel.
+*/
+
+SELECT CodImovel as código, Imovel as descrição FROM Imovel WHERE AreaConstruida BETWEEN 180 and 300 ORDER BY Imovel desc 
+
+
+
+
