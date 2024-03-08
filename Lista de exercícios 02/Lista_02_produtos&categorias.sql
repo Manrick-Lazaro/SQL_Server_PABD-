b3456344107c6047eb6707a86a8a7d62f4fa70f6
manrick Lázaro <manricksantos@gmail.com>
2024-03-08 09:53:01 -0300

    resolve questão 01 da lista 02

---
+++ Lista de exercícios 02/Lista_02_produtos&categorias.sql
@@ -0,0 +1,16 @@
+-- Lista de Exercícios 2 – Produtos e Categorias
+
+/*
+	Exercício 2609 – Produtos por Categoria
+	
+	Como de costume o setor de vendas está fazendo uma análise
+	de quantos produtos temos em estoque, e você poderá ajudar
+	eles.
+
+	Então seu trabalho será exibir o nome e a quantidade de
+	produtos de cada uma categoria.
+*/
+
+SELECT Categories.name as Nome, count(*) as "Quantidade de produtos" FROM Categories
+JOIN Products on (Categories.id = Products.id_categories) GROUP BY Categories.name
+
