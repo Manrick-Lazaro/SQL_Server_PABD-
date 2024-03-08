2adc720f72fe1421a5b3721702c27d983ba36d89
manrick Lázaro <manricksantos@gmail.com>
2024-03-08 10:12:31 -0300

    resolve questão 02 da lista 01

--- Lista de exercícios 02/Lista_02_produtos&categorias.sql
+++ Lista de exercícios 02/Lista_02_produtos&categorias.sql
@@ -14,3 +14,24 @@
 SELECT Categories.name as Nome, count(*) as "Quantidade de produtos" FROM Categories
 JOIN Products on (Categories.id = Products.id_categories) GROUP BY Categories.name
 
+
+
+
+
+
+/*
+	Exercício 2617 – Fornecedor Ajax SA
+
+	O setor financeiro encontrou alguns problemas na entrega de
+	um dos nossos fornecedores, a entrega dos produtos não condiz
+	com a nota fiscal.
+
+	Seu trabalho é exibir o nome dos produtos e o nome do
+	fornecedor, para os produtos fornecidos pelo fornecedor ‘Ajax
+	SA’.
+*/
+
+SELECT Products.name, Providers.name FROM Products 
+JOIN Providers ON Products.id_providers = Providers.id
+WHERE Providers.name like 'Ajax SA'
+
